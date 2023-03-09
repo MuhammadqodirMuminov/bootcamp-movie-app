f70d2f19ec42a17985bd3fddddcb5e1335f326d5
muhammad1749 <muhammadqodirmuminov2@gmail>
2023-03-09 17:40:37 +0500

    hero is fully graduated

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,6 +4,7 @@ import { BASE_IMAGE_URL } from "src/helpers";
 import { IMovie } from "src/interfaces/app.interface";
 import { HeroProps } from "./hero.props";
 import { TbPlayerPlay } from "react-icons/tb";
+import ReactStars from "react-stars";
 
 const Hero = ({ trending }: HeroProps): JSX.Element => {
 	const [movie, setMovie] = useState<IMovie>({} as IMovie);
@@ -16,7 +17,7 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
 	}, [trending]);
 
 	return (
-		<div className="flex flex-col space-y-2 py-20 md:space-y-4 lg:h-[65vh] lg:pb-12 lg:justify-end">
+		<div className="flex flex-col space-y-2 py-20 md:space-y-4 lg:h-[65vh] lg:pb-12 lg:justify-center">
 			<div className="  absolute top-0 left-0 -z-10  h-[95vh] w-full">
 				<Image
 					src={`${BASE_IMAGE_URL}${
@@ -28,12 +29,27 @@ const Hero = ({ trending }: HeroProps): JSX.Element => {
 				/>
 			</div>
 
+			<div className="py-[4px] px-[8px] text-center w-[111px] bg-[#1d1d1d]/50 rounded-tr-[8px] rounded-bl-[8px]">
+				{movie?.media_type}
+			</div>
+
+			<div className="flex items-center space-x-4">
+				<ReactStars
+					count={10}
+					value={movie?.vote_average}
+					color2={"#fff"}
+					edit={false}
+					size={32}
+				/>
+				<p>({movie?.vote_count})</p>
+			</div>
+
 			<h1 className="text-2xl font-bold md:text-4xl lg:text-7xl ">
 				{movie?.name || movie?.original_name || movie?.original_title}
 			</h1>
 
 			<p className=" max-w-xs md:max-w-lg lg:max-w-2xl text-xs md:text-lg lg:text-2xl text-shadow-md">
-				{movie?.overview}
+				{movie?.overview?.slice(0, 100)}...
 			</p>
 
 			<div className="w-[200px] h-[56px] font-bold bg-white/40 text-black  space-x-2 rounded-full flex items-center justify-center">
